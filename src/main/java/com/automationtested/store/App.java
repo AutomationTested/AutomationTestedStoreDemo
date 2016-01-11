2cb5727f1b0bdafc703d6f8e3ebbf05a3002ae46
automationtested <automationtested@gmail.com>
2016-01-10 16:43:06 -0800

    Initial commit

---
+++ src/main/java/com/automationtested/store/App.java
@@ -0,0 +1,13 @@
+package com.automationtested.store;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
