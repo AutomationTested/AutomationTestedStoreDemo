55f3b423c21380749de7c3f454f446fb9fe74ba6
automationtested <automationtested@gmail.com>
2016-01-17 20:24:51 -0800

    Added additional dependencies to the pom.xml file and unoptimized automation code to the AppTest class file.

--- src/test/java/com/automationtested/store/AppTest.java
+++ src/test/java/com/automationtested/store/AppTest.java
@@ -1,38 +1,55 @@
 package com.automationtested.store;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest {
+
+    @Test
+    public static void Login() throws InterruptedException {
+
+        System.out.println("Test started!");
+
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+
+        //Manually move window to to secondary screen for demo!
+        Thread.sleep(5000);
+
+        driver.navigate().to("http://store1.automationtested.com/");
+
+        //Sleep statements were added to visually slow the automation for demonstration purposes.
+        Thread.sleep(2000);
+        driver.findElement(By.partialLinkText("Log in")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("Email")).sendKeys("tonystark@gmail.com");
+        Thread.sleep(2000);
+        driver.findElement(By.id("Password")).sendKeys("Password123");
+        Thread.sleep(2000);
+
+
+        //Using the class name does not find the element.
+        //driver.findElement(By.className("button-1 login-button")).click();
+
+        //Click on the "LOG IN" button.
+        driver.findElement(By.cssSelector(".button-1.login-button")).click();
+
+        Thread.sleep(2000);
+
+
+        driver.findElement(By.partialLinkText("Log out")).click();
+        Thread.sleep(2000);
+
+        System.out.println("Login test passed.");
+
+        //Close the active session and FireFox browser currently in use.
+        driver.close();
+
+        //Closes all windows opened by WebDriver.
+        driver.quit();
     }
 }
